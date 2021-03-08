fc430b34ba613b61515cf9177ae768d9df8c5223
Nikhil <ndstik@gmail.com>
2021-03-07 23:28:01 -0800

    pro

---
+++ README.md
@@ -0,0 +1,3 @@
+# Terrible server using node
+Have you ever wanted to run a server that barely does anything? Me too.  
+This is a bare bones skeleton of a skeleton of getting started with making a node.js based web server.  
---
+++ index.js
@@ -0,0 +1,29 @@
+
+const express = require('express')
+const app = express()
+
+const port = 3030
+
+
+// Start the server using app.listen()
+
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.listen(port, () => {
+    console.log("listening on 3030")
+})
+
+app.get('/', (req, res) => {
+    res.send("home")
+})
+app.get('/foo', (req, res) => {
+    res.json({ "foo": "uh"})
+})
+
+app.post('/bar', (req, res) => {
+    let body = req.body
+    // foo is the key we made in the previous function
+    res.send(req.body.foo)
+})
